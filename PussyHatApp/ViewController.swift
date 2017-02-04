7b4420f7db02dfae1eece4af3b31cd22579062bb
Sam Showalter <samwam2@gmail.com>
2017-02-04 12:11:52 -0800

    first Commit

---
+++ PussyHatApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PussyHatApp
+//
+//  Created by Sam Showalter on 2/4/17.
+//  Copyright © 2017 Sam Showalter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
