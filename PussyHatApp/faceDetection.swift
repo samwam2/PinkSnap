7b6d0e5fc551d5a13294989a692147a45dbfdd33
Sam Showalter <samwam2@gmail.com>
2017-02-04 20:32:32 -0800

    face working somewhat

---
+++ PussyHatApp/faceDetection.swift
@@ -0,0 +1,99 @@
+//
+//  faceDetection.swift
+//  PussyHatApp
+//
+//  Created by ShowalterS18 on 2/4/17.
+//  Copyright © 2017 Sam Showalter. All rights reserved.
+//
+
+import Foundation
+import CoreImage
+import UIKit
+
+class faceDetection: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+   
+      @IBOutlet var imageView: UIImageView!
+    
+    @IBAction func takePhoto(_ sender: Any) {
+        if !UIImagePickerController.isSourceTypeAvailable(.camera) {
+            return
+        }
+        
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .camera
+        
+        present(imagePicker, animated: true, completion: nil)
+        
+        //imagePickerController(imagePicker, didFinishPickingMediaWithInfo: "no")
+    }
+    
+    let imagePicker = UIImagePickerController()
+    var imageHat = #imageLiteral(resourceName: "hat")
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.contentMode = .scaleAspectFit
+            imageView.image = pickedImage
+        }
+        
+        dismiss(animated: true, completion: nil)
+        self.detect()
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func detect() {
+        let imageOptions =  NSDictionary(object: NSNumber(value: 5) as NSNumber, forKey: CIDetectorImageOrientation as NSString)
+        let personciImage = CIImage(cgImage: imageView.image!.cgImage!)
+        let accuracy = [CIDetectorAccuracy: CIDetectorAccuracyHigh]
+        let faceDetector = CIDetector(ofType: CIDetectorTypeFace, context: nil, options: accuracy)
+        let faces = faceDetector?.features(in: personciImage, options: imageOptions as? [String : AnyObject])
+        
+        if let face = faces?.first as? CIFaceFeature {
+            print("found bounds are \(face.bounds)")
+            
+            let alert = UIAlertController(title: "Say Cheese!", message: "We detected a face!", preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+            
+            if face.hasSmile {
+                print("face is smiling");
+            }
+            
+            if face.hasLeftEyePosition {
+                print("Left eye bounds are \(face.leftEyePosition)")
+            }
+            
+            if face.hasRightEyePosition {
+                print("Right eye bounds are \(face.rightEyePosition)")
+            }
+        } else {
+            let alert = UIAlertController(title: "No Face!", message: "No face was detected", preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+
+    func showPicture() {
+       var imageThing = imagePicker
+        //imageView.image = imageThing
+    }
+    
+    override func viewDidLoad() {
+        imagePicker.delegate = self
+
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+      
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+} // last clossing braket for the class
